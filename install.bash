ff4edca6d010282cdc6ffc19b2dcf41d4ec67a9f
kamontat <kamontat_c@hotmail.com>
2017-05-12 01:41:00 +0700

    add remove feature

--- install.bash
+++ install.bash
@@ -10,4 +10,12 @@ read -n 1 ans
 if [[ $ans == "Y" ]]; then
     ./tester
 fi
+
+printf "Do you want to delete git[Y|n]? "
+read -n 1 ans
+
+if [[ $ans == "Y" ]]; then
+    sudo rm -rf .git 
+fi
+
 echo "complete!"
