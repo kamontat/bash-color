91a2bdbc78ab113ea28dd7190525e2fbc187127c
kamontat <kamontat_c@hotmail.com>
2017-05-12 16:24:49 +0700

    add new line

--- install.bash
+++ install.bash
@@ -5,21 +5,21 @@ chmod 755 ./color_constants
 chmod 755 ./tester
 chmod 755 ./example_usage
 
-printf "Do you want to test[Y|n]? "
+printf "\nDo you want to test[Y|n]? "
 read -n 1 ans
 
 if [[ $ans == "Y" ]]; then
     ./example_usage
 fi
 
-printf "Do you want to delete git[Y|n]? "
+printf "\nDo you want to delete git[Y|n]? "
 read -n 1 ans
 
 if [[ $ans == "Y" ]]; then
     sudo rm -rf .git 
 fi
 
-printf "Do you want to minimum folder[Y|n]? "
+printf "\nDo you want to minimum folder[Y|n]? "
 read -n 1 ans
 
 if [[ $ans == "Y" ]]; then
