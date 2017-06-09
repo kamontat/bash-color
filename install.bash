ff1588fe903974076a3555cadd14fda012861f67
kamontat <kamontat_c@hotmail.com>
2017-06-09 16:22:23 +0700

    minify code and test

--- install.bash
+++ install.bash
@@ -1,32 +1,25 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-chmod 755 ./color_constants
-chmod 755 ./tester
-chmod 755 ./example_usage
+chmod 755 ./color_constants.sh      # new version
+chmod 755 ./color_raw_constants.sh  # old version
 
-printf "\nDo you want to test[Y|n]? "
-read -n 1 ans
+source ./color_constants.sh
 
-if [[ $ans == "Y" ]]; then
-    ./example_usage
+exit_code=$?
+if [ $exit_code -eq 2 ]; then
+    exit 2
+elif [ $exit_code -ne 0 ]; then
+    echo "install wrong with some reason"
+    exit $exit_code
 fi
 
-printf "\nDo you want to delete git[Y|n]? "
+printf "\nDo you want to minify folder[Y|n]? "
 read -n 1 ans
-
-if [[ $ans == "Y" ]]; then
-    sudo rm -rf .git 
-fi
-
-printf "\nDo you want to minimum folder[Y|n]? "
-read -n 1 ans
-
-if [[ $ans == "Y" ]]; then
-    rm -rf ./example_usage ./tester ./README.md ./install.bash
-fi
-
-
-source ./color_constants
 echo ""
-printf "$BLINK_RED \bcomplete!$RESET\n"
+
+if [[ $ans == "Y" ]]; then
+    rm -rf ./README.md ./install.bash
+fi
+
+printf "${C_BG_5}${C_FG_7}complete!$C_RE\n"
