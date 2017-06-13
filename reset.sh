1a48fa9acc9d4a67132480123897872faa798b34
kamontat <kamontat_c@hotmail.com>
2017-06-13 17:19:07 +0700

    add unset script to reset color loading for disable color option

---
+++ reset.sh
@@ -0,0 +1,28 @@
+if [ ! -z $C_COMPLETE ]; then
+    DEFAULT="C_"
+    FORGROUND="FG_"
+    BACKGROUND="BG_"
+
+    C_BO=""
+    C_DI=""
+    C_UL=""
+    C_RV=""
+    C_SM=""
+    C_BL=""
+    C_IV=""
+
+    # reset variable
+    C_RE_AL=""
+    C_RE_SM=""
+    C_RE_UL=""
+
+    for ((i=0; i<$C_COMPLETE; i++)); do
+        temp="$DEFAULT$FORGROUND$i"
+        declare $temp=""
+
+        temp="$DEFAULT$BACKGROUND$i"
+        declare $temp=""
+    done
+fi
+
+export C_COMPLETE=
