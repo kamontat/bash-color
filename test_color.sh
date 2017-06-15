d40c9365e89d77b3e4c3e1e76d854bd3f04e34c9
kamontat <kamontat_c@hotmail.com>
2017-06-15 18:29:08 +0700

    change test name, and behavior

---
+++ test_color.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+else
+    echo "not support tput, use color_raw_constant.sh instead."
+    exit 2
+fi
+
+function run {
+    f=$1
+    b=$2
+    printf "$(tput setab $b)$(tput setaf $f)%s(b=%3d,f=%3d)$(tput sgr0)" "CODE" "$b" "$f"
+}
+
+function all {
+    for ((i=0; i<=$ncolors; i++)); do
+        for ((j=0; j<=$ncolors; j++)); do
+            run $j $i
+            [[ $(($j % 8)) -eq 0 ]] && echo ""
+        done
+    done
+}
+
+function fixed_f {
+    for ((i=0; i<=$ncolors; i++)); do
+        run $1 $i
+        [[ $(($i % 8)) -eq 0 ]] && echo ""
+    done
+}
+
+function fixed_b {
+    for ((i=0; i<=$ncolors; i++)); do
+        run $i $1
+        [[ $(($i % 8)) -eq 0 ]] && echo ""
+    done
+}
+
+fore=-1
+back=-1
+HELP="
+Testing color, you must manually source color.
+'none' option is mean log all possible color in your app/OS, 
+beware this may very long 
+availble options
+    f - foreground color (use number to set)
+    b - background color (use number to set)
+----------------------------------------------
+create by: Kamontat Chantrachirathumrong
+version:   1.0.0
+"
+
+while getopts 'f:b:h' flag; do
+    case "${flag}" in
+        f) fore="$OPTARG" ;;
+        b) back="$OPTARG" ;;
+        h) echo "$HELP" && exit 0 ;;
+    esac
+done
+
+if [[ $fore -lt 0 ]] && [[ $back -lt 0 ]]; then
+    all
+elif [[ $fore -gt 0 ]] && [[ $back -gt 0 ]]; then
+    run $fore $back
+    echo ""
+elif [[ $fore -gt 0 ]]; then 
+    fixed_f $fore
+elif [[ $back -gt 0 ]]; then
+    fixed_b $back
+fi
