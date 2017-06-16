0415471c264434e150e17cae2ac423acc805cc40
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-06-16 23:28:04 +0700

    new param instants

--- color_util.sh
+++ color_util.sh
@@ -21,11 +21,11 @@ function get_name_from_url {
     echo "${1##*/}"
 }
 
-if [[ $1 = "load" ]]; then
+if [[ $1 = "load" || $1 = "li" ]]; then
     [ -f $LOCATION/color_constants.sh ] && cat $LOCATION/color_constants.sh || echo "$(load $CONS)"
-elif [[ $1 = "clear" ]]; then
+elif [[ $1 = "remove" || $1 = "rm" ]]; then
     [ -f $LOCATION/color_reset.sh ] && cat $LOCATION/color_reset.sh || echo "$(load $RESET)"
-elif [[ $1 = "delete" ]]; then
+elif [[ $1 = "clear" || $1 = "cl" ]]; then
     [ -f $LOCATION/color_constants.sh ] && rm -f $LOCATION/color_constants.sh
     [ -f $LOCATION/color_reset.sh ] && rm -f $LOCATION/color_reset.sh
 fi
